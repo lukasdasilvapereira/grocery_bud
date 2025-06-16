002adabbbc2aacca6dac75bd5a3c79af30d3cbcc
Lucas Pereira <lucasdasilvapereira021@gmail.com>
2025-06-16 19:00:00 -0300

    .

--- scripts.js
+++ scripts.js
@@ -1,12 +1,27 @@
-let inputEl = document.querySelector(".input-el").value;
-let heroEl = document.querySelector(".hero-el")
+let inputEl = document.querySelector(".input-el");
+let heroEl = document.querySelector(".hero-el");
+let clearItems = document.querySelector(".clear")
+let count = 0;
 
 function submit() {
-    heroEl.innerHTML = `<div class="hero">
-                    <p class="text">Bacon</p>
+    heroEl.innerHTML += `<div class="hero hero${count + 1}">
+                    <p class="text">${inputEl.value}</p>
                     <div>
                         <input type="checkbox" name="checkbox" id='checkbox' checked>
                         <i class="fa-solid fa-trash trash"onclick="deleteItem()"></i>
                     </div>
                 </div>`
+                inputEl.value = ""
+                count++
 }
+
+function deleteItem() {
+    let trash = document.querySelector(".fa-trash")
+    heroEl(`hero${count}`).innerHTML = ""
+}
+
+
+clearItems.addEventListener("click", function() {
+    heroEl.textContent = ""
+    count = 0;
+})
